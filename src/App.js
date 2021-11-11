58dc6e741fda123b034a0748d8dfbf92cdd995ed
asceznyk <asceznyk@gmail.com>
2021-11-11 15:34:56 +0530

    some crap

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          OK this looks good (locally!)
         </a>
       </header>
     </div>
