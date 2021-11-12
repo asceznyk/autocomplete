0f47e9ee8703d3bb3745138ded227dc0f8589faa
asceznyk <asceznyk@gmail.com>
2021-11-12 12:20:00 +0530

    sample crap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { ShoppingCartRounded } from '@material-ui/icons/';
+import MenuIcon from '@mui/icons-material/Menu';
 
 import { 
   Alert, 
@@ -9,21 +10,48 @@ import {
   FormControl, 
   FormHelperText, 
   InputLabel, 
-  Input 
+  Input,
+  AppBar,
+  Box, 
+  Toolbar,
+  Typography,
+  IconButton,
 } from '@mui/material';
 
-import logo from './logo.svg';
 import './App.css';
 
-class Header extends React.Component {
-  render() {
-    return <h1>{this.props.name}</h1>
-  }
+function Header(props) {
+  return <h1>{props.name}</h1>
+}
+
+function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 function App() {
   return (
     <div className="App">
+      <ButtonAppBar/>
       <Header name="SimpleForms"/>
       <FormControl>
       <InputLabel htmlFor="my-input">Email address</InputLabel>
