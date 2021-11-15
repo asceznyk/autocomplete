d40b95427a3049027d2f8428f0c34ef8b2e9252d
asceznyk <asceznyk@gmail.com>
2021-11-15 13:35:32 +0530

    ..

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+//const port = process.env.PORT || 8080;
 
 app.post('/', function (req, res) {
   const query = req.body.query;
   res.send(query);
 });
 
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log('server started at http://localhost:' + port);
 });
-
--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function Header() {
 }
 
 async function getMovieData(query) {
-  const response = await fetch('http://104.154.31.26:3000/movies', {
+  const response = await fetch('http://104.154.31.26:3000/', {
     method:'POST',
     headers: {
       'Accept':'application/json',
