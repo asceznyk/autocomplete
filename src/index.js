667e2f1438d7fc967fe78281fe9aaf82cdf6201f
asceznyk <asceznyk@gmail.com>
2021-11-15 13:22:43 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {Helmet} from 'react-helmet';
 
 import './index.css';
 import App, {Title} from './App';
-import './Api'
+//import './Api'
 import reportWebVitals from './reportWebVitals';
 
 class TitleBar extends React.PureComponent {
