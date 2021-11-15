907a42d0a2b97432e1c876a61f0d06ccd04ef768
asceznyk <asceznyk@gmail.com>
2021-11-15 15:01:00 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
-
-console.log('something!')
+const port = 3000;
 
 app.get('/', function (req, res) {
   res.send('Hi there!');
