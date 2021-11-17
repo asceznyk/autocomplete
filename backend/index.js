608cbc554eca187b5dac1e8d01991a21af1c3932
asceznyk <asceznyk@gmail.com>
2021-11-17 12:06:01 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.send('Hellooo!');
