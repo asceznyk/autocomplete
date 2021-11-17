6ee4983e4b077d638e20b93ea75cc646578259c9
asceznyk <asceznyk@gmail.com>
2021-11-17 12:15:55 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.get('/', function (req, res) {
@@ -11,7 +14,8 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  const query = req.body.query;
+  const query = req;
+  console.log(req);
   res.send(query);
 });
 
