669f907725507ffe6c1c9a6a6bdbbae151b42c9d
asceznyk <asceznyk@gmail.com>
2021-12-08 13:12:40 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -41,8 +41,8 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.get('/', function (req, res) {
-  content = selectData();
+app.get('/', async (req, res) => { 
+  content = await selectData();
   res.json(content);
 });
 
