dab6290788b1b95dd4a5c123d245ac3f05e7f879
asceznyk <asceznyk@gmail.com>
2021-12-08 14:29:27 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -92,7 +92,6 @@ function App() {
   }
 
   function handleLoad() {
-
     fetch('http://35.193.28.105:5000', {
       method:'GET',
       headers: {
@@ -173,7 +172,6 @@ function App() {
         </Box>
       </Box> 
       {content.map((row, index) => (
-        console.log(index, row.query),
         <OutlinedCard key={index} header= {row.query} body = {row.query} />
       ))}
       </Container>
