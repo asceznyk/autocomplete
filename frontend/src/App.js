2052b48d004964982c60417fd28f5faaa4555dc9
asceznyk <asceznyk@gmail.com>
2021-12-13 12:52:24 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -81,7 +81,7 @@ function OutlinedCard(props) {
 function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="fixed" color="secondary">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>{ Title }</Typography>
       </Toolbar>
