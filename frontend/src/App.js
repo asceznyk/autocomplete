bc8c27dfbd5187ce7d232dcfc9a282963c6e3cd4
asceznyk <asceznyk@gmail.com>
2021-12-14 15:44:28 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -168,7 +168,7 @@ export default function App() {
             renderOption={(props, option) => (
               <Box {...props} ><Stack spacing={2} direction="row">
                 <Typography variant="body2">{option.label}</Typography> 
-                <Chip label={option.year} color="primary"/>
+                <Chip label={option.year} color="primary" size="small" variant="outlined"/>
               </Stack></Box>
             )}
             freeSolo
