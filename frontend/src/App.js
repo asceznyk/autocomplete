0646198de16a7aec14025cbae67866ba35b80e70
asceznyk <asceznyk@gmail.com>
2021-12-14 14:27:19 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -165,7 +165,7 @@ export default function App() {
               return option.label;
             }}
             renderOption={(props, option) => (
-              <Box {...props} >{option.label} <Chip>{option.year}</Chip></Box>)}
+              <Box {...props} >{option.label} <Chip label={option.year}/></Box>)}
             freeSolo
             renderInput={(params) => <TextField {...params} label="Movie" color="secondary"/>}
           />
